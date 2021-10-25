de6318a7963182ce330683d97deab4b7eaa10e94
Diêgo Ferreira <diegofrr.19@gmail.com>
2021-10-25 17:10:47 -0300

    Initial commit

---
+++ src/test/java/com/projetodsc/ProjetoDscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetodsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
