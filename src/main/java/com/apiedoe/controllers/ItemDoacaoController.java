7015e06dc66997cb19df831f81eaeebdcf4d0073
Diêgo Ferreira <diegofrr.19@gmail.com>
2021-11-21 00:18:58 -0300

    att

--- src/main/java/com/apiedoe/controllers/ItemDoacaoController.java
+++ src/main/java/com/apiedoe/controllers/ItemDoacaoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.apiedoe.models.Descritor;
-import com.apiedoe.models.Doacao;
 import com.apiedoe.models.TipoItem;
 import com.apiedoe.models.dtos.DoacaoDTO;
 import com.apiedoe.models.requestModels.ItemRequest;
