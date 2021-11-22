ad8b043d388eb04a9e58a5129217063bbda7c3f1
Diêgo Ferreira <diegofrr.19@gmail.com>
2021-11-22 10:33:02 -0300

    atualização no formato da data

--- src/main/java/com/apiedoe/models/responseModels/DoacaoResponse.java
+++ src/main/java/com/apiedoe/models/responseModels/DoacaoResponse.java
@@ -1,6 +1,8 @@
 package com.apiedoe.models.responseModels;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 import com.apiedoe.models.Doacao;
 
@@ -13,13 +15,16 @@ public class DoacaoResponse {
 	private int quantidadeDoacao;
 	private String data;
 
-	public DoacaoResponse() {}
-	
+	public DoacaoResponse() {
+	}
+
 	public DoacaoResponse(Doacao dadosDoacao) {
 		this.idItemDoacao = dadosDoacao.getIdItemDoacao();
 		this.idItemNecessario = dadosDoacao.getIdItemNecessario();
 		this.quantidadeDoacao = dadosDoacao.getQuantidadeDoacao();
-		this.data = new SimpleDateFormat("dd 'de' MMMM 'de' yyyy hh:mm:ss a").format(dadosDoacao.getData()).toString();
+		DateFormat df = new SimpleDateFormat("dd 'de' MMMM 'de' yyyy hh:mm:ss a");
+		df.setTimeZone(TimeZone.getDefault());
+		this.data = df.format(dadosDoacao.getData());
 	}
 
 }
